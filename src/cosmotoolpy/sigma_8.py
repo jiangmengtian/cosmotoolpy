d14a2d40150ec3f193e0a9b79116743149f7b0c0
Jiang Mengtian <jiangmengtian@JiangdeMacBook-Pro.local>
2025-09-06 13:40:52 +0800

    Add annotations to sigma_8.py

--- src/cosmotoolpy/sigma_8.py
+++ src/cosmotoolpy/sigma_8.py
@@ -1,11 +1,26 @@
 import numpy as np
 from scipy import integrate
 
-def sigma_R(P_interpolate, R=8):
+def sigma_R(P_interpolate: interp1d, R: int = 8) -> float:
+    '''
+    Compute sigma8 from the interpolated power spectrum
+
+    Parameters
+    ----------
+    P_interpolate: interp1d
+        An interp1d instance generated with k and P(k)
+    R: int
+        Radius of the spherical top-hat window function
+
+    Returns
+    -------
+    sigma: float
+        Sigma8 value
+    '''
     pi = np.pi
     
     def integral(k):
-        p = P_interpolate(k).item()
+        p = P_interpolate(k)
         return k*k*p*(3*(np.sin(k*R)-k*R*np.cos(k*R))/k/R/k/R/k/R)**2
         
     sigma, err = integrate.quad(integral, 0, np.inf, limit=100, epsrel=1e-4)
