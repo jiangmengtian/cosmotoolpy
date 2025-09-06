368e914f6c0206ac4e7809d3cb74c3ee9d8537fa
Jiang Mengtian <jiangmengtian@JiangdeMacBook-Pro.local>
2025-09-06 14:39:47 +0800

    Add annotations to binning_correction.py

--- src/cosmotoolpy/binning_correction.py
+++ src/cosmotoolpy/binning_correction.py
@@ -1,17 +1,37 @@
+from typing import Tuple
 import numpy as np
-import scipy.constants as C
-from mycosmotoolpy import cpseh
+from cosmotoolpy import cpseh
 
-def binned_power_spectrum(P_interpolate, Ngrid, L=1000):
-    pi = C.pi
+def binning_correction(P_interpolate: interp1d, Ngrid: int, L: float = 1000) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    '''
+    Compute the binning correction for the power spectrum
+
+    Parameters
+    ----------
+    P_interpolate: interp1d
+        An interp1d instance generated from arrays k and P(k)
+    Ngrid: int
+        Number of grids
+    L: float, optional
+        Box size
+
+    Returns
+    -------
+    k: 1d-array
+        Wavenumber of each bin
+    Pk: 1d-array
+        Averaged power spectrum of each bin
+    Nk: 1d-array
+        Number of independent modes in each bin
+    '''
+    pi = np.pi
     V  = L*L*L
-    kF = 2*pi/L
     
-    nx_axis = np.fft.fftfreq(Ngrid, 1/Ngrid)
-    ny_axis = np.fft.fftfreq(Ngrid, 1/Ngrid)
-    nz_axis = np.fft.rfftfreq(Ngrid, 1/Ngrid)
-    ny, nx, nz = np.meshgrid(nx_axis, ny_axis, nz_axis)
-    k_modulus = (nx*nx+ny*ny+nz*nz)**(1/2)*kF
-    delta_k = (V*P_interpolate(k_modulus))**(1/2)
+    nx_axis = np.fft.fftfreq(Ngrid, L/Ngrid)
+    ny_axis = np.fft.fftfreq(Ngrid, L/Ngrid)
+    nz_axis = np.fft.rfftfreq(Ngrid, L/Ngrid)
+    nx, ny, nz = np.meshgrid(nx_axis, ny_axis, nz_axis, indexing='ij', sparse=True)
+    k_modulus = 2*pi*np.sqrt(nx*nx+ny*ny+nz*nz)
+    delta_k = np.sqrt(V*P_interpolate(k_modulus))
     
     return cpseh.power_spectrum_estimator(delta_k.astype(np.complex128), Ngrid)
