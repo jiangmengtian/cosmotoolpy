d58f9394bcf297c3fd7e7e191a87167ec8aea4f3
Jiang Mengtian <jiangmengtian@JiangdeMacBook-Pro.local>
2025-09-06 16:19:44 +0800

    Add annotations to initial_condition.py and optimize the code

--- src/cosmotoolpy/initial_condition.py
+++ src/cosmotoolpy/initial_condition.py
@@ -1,21 +1,40 @@
 from random import gauss
 import numpy as np
 
-def ic_gaussian_random_field(P_interpolate, Ngrid, L=1000, mu=0, sigma=1):
+def gaussian_random_field(P_interpolate: interp1d, Ngrid: int, L: float = 1000, mu: float = 0, sigma: float = 1) -> np.ndarray:
+    '''
+    Generate a gaussian random field from a given power spectrum
+
+    Parameters
+    ----------
+    P_interpolate: interp1d
+        An interp1d instance generated from arrays k and P(k)
+    Ngrid: int
+        Number of grids
+    L: float, optional
+        Box size
+    mu: float, optional
+        Mean of the gaussian distribution
+    sigma: float, optional
+        Standard deviation of the gaussian distribution
+
+    Returns
+    -------
+    GRFk: 3d-array
+        Gaussian random field in Fourier space
+    '''
     pi = np.pi
     V  = L*L*L
-    kF = 2*pi/L
-    normal_factor = 1/(sigma*sigma*V*V/Ngrid**3)**(1/2)
+    normal_factor = 1/np.sqrt(sigma*sigma*V*V/Ngrid**3)
     
-    GRFx = np.random.normal(mu, sigma, Ngrid*Ngrid*Ngrid)
-    GRFx = np.reshape(GRFx, (Ngrid, Ngrid, Ngrid))
+    GRFx = np.random.normal(mu, sigma, size=(Ngrid, Ngrid, Ngrid))
     GRFk = np.fft.rfftn(GRFx)*V/Ngrid/Ngrid/Ngrid
-    nx_axis = np.fft.fftfreq(Ngrid, 1/Ngrid)
-    ny_axis = np.fft.fftfreq(Ngrid, 1/Ngrid)
-    nz_axis = np.fft.rfftfreq(Ngrid, 1/Ngrid)
-    ny, nx, nz = np.meshgrid(nx_axis, ny_axis, nz_axis)
-    k_modulus = (nx*nx+ny*ny+nz*nz)**(1/2)*kF
+    nx_axis = np.fft.fftfreq(Ngrid, L/Ngrid)
+    ny_axis = np.fft.fftfreq(Ngrid, L/Ngrid)
+    nz_axis = np.fft.rfftfreq(Ngrid, L/Ngrid)
+    nx, ny, nz = np.meshgrid(nx_axis, ny_axis, nz_axis, indexing='ij', sparse=True)
+    k_modulus = 2*pi*np.sqrt(nx*nx+ny*ny+nz*nz)
     P_fit = P_interpolate(k_modulus)
-    GRFk = GRFk*normal_factor*(V*P_fit)**(1/2)
+    GRFk = GRFk*normal_factor*np.sqrt(V*P_fit)
 
     return GRFk
