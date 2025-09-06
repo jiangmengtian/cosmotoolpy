838d05d280fea88969a4d7090ff1e7da69c483e0
Jiang Mengtian <jiangmengtian@JiangdeMacBook-Pro.local>
2025-09-06 14:17:37 +0800

    Adjust integral interval and relative error

--- src/cosmotoolpy/sigma_8.py
+++ src/cosmotoolpy/sigma_8.py
@@ -23,7 +23,7 @@ def sigma_R(P_interpolate: interp1d, R: int = 8) -> float:
         p = P_interpolate(k)
         return k*k*p*(3*(np.sin(k*R)-k*R*np.cos(k*R))/k/R/k/R/k/R)**2
         
-    sigma, err = integrate.quad(integral, 0, np.inf, limit=100, epsrel=1e-4)
+    sigma, err = integrate.quad(integral, P_interpolate.x[0], P_interpolate.x[-1], limit=100, epsrel=1e-5)
     sigma = (sigma/2/pi/pi)**(1/2)
     
     return sigma
