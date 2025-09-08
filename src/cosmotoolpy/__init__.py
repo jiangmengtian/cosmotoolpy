a35d377b50415a8bf31ce99acb81e8a790371dad
Jiang Mengtian <jiangmengtian@JiangdeMacBook-Pro.local>
2025-09-08 15:26:29 +0800

    Update __init__.py

--- src/cosmotoolpy/__init__.py
+++ src/cosmotoolpy/__init__.py
@@ -2,3 +2,4 @@ from .linear_growth_factor import lgf
 from .power_spectrum import PowerSpectrum
 from .cpse import power_spectrum_estimator
 from .cpaint import ngp_interlace, cic_interlace
+from .zeldovich_approximation import zeldovich_approximation
