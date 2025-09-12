c0b25a61af3c92690795ef6944914e9b6602e621
Jiang Mengtian <jiangmengtian@JiangdeMacBook-Pro.local>
2025-09-13 00:17:23 +0800

    Add the normalization factor in irfftn and a variable z_init

--- src/cosmotoolpy/zeldovich_approximation.py
+++ src/cosmotoolpy/zeldovich_approximation.py
@@ -1,7 +1,9 @@
 from .power_spectrum import PowerSpectrum
+from typing import Tuple
 import numpy as np
+from .linear_growth_factor import linear_growth_factor
 
-def zeldovich_approximation(pow_spec: PowerSpectrum, Ngrid: int, L: float = 1000) -> np.ndarray:
+def zeldovich_approximation(pow_spec: PowerSpectrum, Ngrid: int, z_init: int, Omega_m: float = 0.23, Omega_lambda: float = 0.77, L: float = 1000) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     '''
     Generate initial condition using Zeldovich approximation
 
@@ -10,19 +12,29 @@ def zeldovich_approximation(pow_spec: PowerSpectrum, Ngrid: int, L: float = 1000
     pow_spec: PowerSpectrum
     Ngrid: int
         Number of grids
+    z_init: int
+        Initial redshift
+    Omega_m: float, optional
+        Proportion of matter component today
+    Omega_lambda: float, optional
+        Proportion of dark energy component today
     L: float, optional
         Box size
 
     Returns
     -------
-    position: 3d-array
-        Position of particles
+    psix: 3d-array
+        Displacement of particles in x-direction
+    psiy: 3d-array
+        Displacement of particles in y-direction
+    psiz: 3d-array
+        Displacement of particles in z-direction
     '''
     pi = np.pi
     V  = L*L*L
     kF = 2*pi/L
 
-    delta_k = pow_spec.get_initial_condition(Ngrid)
+    delta_k = pow_spec.get_initial_condition(Ngrid)*linear_growth_factor(Omega_m, Omega_lambda, z_init)
     nx_axis = np.fft.fftfreq(Ngrid, 1/Ngrid)
     ny_axis = np.fft.fftfreq(Ngrid, 1/Ngrid)
     nz_axis = np.fft.rfftfreq(Ngrid, 1/Ngrid)
@@ -30,14 +42,8 @@ def zeldovich_approximation(pow_spec: PowerSpectrum, Ngrid: int, L: float = 1000
     k_modsq = (nx*nx+ny*ny+nz*nz)*kF*kF
     inv_k_modsq = np.divide(1.0, k_modsq, out=np.zeros_like(k_modsq, dtype=float), where=k_modsq!=0)
     factor = 1j*delta_k*kF
-    psi_x = np.fft.irfftn(factor*nx*inv_k_modsq) / V
-    psi_y = np.fft.irfftn(factor*ny*inv_k_modsq) / V
-    psi_z = np.fft.irfftn(factor*nz*inv_k_modsq) / V
-
-    nx_axis = np.arange(Ngrid)
-    ny_axis = np.arange(Ngrid)
-    nz_axis = np.arange(Ngrid)
-    grid = np.stack(np.meshgrid(nx_axis, ny_axis, nz_axis, indexing="ij"), axis=-1)*(L/Ngrid)
-    position = grid.reshape(-1, 3) + np.stack((psi_x, psi_y, psi_z), axis=-1).reshape(-1, 3)
+    psix = np.fft.irfftn(factor*nx*inv_k_modsq)/V*(Ngrid*Ngrid*Ngrid)
+    psiy = np.fft.irfftn(factor*ny*inv_k_modsq)/V*(Ngrid*Ngrid*Ngrid)
+    psiz = np.fft.irfftn(factor*nz*inv_k_modsq)/V*(Ngrid*Ngrid*Ngrid)
     
-    return position
+    return psix, psiy, psiz
