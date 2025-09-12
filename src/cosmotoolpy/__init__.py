6f3f757b6fef86449daac7aa6a7d13933d8de20d
Jiang Mengtian <jiangmengtian@JiangdeMacBook-Pro.local>
2025-09-13 00:14:27 +0800

    Rename the function linear_growth_factor

--- src/cosmotoolpy/__init__.py
+++ src/cosmotoolpy/__init__.py
@@ -1,4 +1,4 @@
-from .linear_growth_factor import lgf
+from .linear_growth_factor import linear_growth_factor
 from .power_spectrum import PowerSpectrum
 from .cpse import power_spectrum_estimator
 from .cpaint import ngp_interlace, cic_interlace
--- src/cosmotoolpy/linear_growth_factor.py
+++ src/cosmotoolpy/linear_growth_factor.py
@@ -1,16 +1,16 @@
 import numpy as np
 from scipy import integrate
 
-def lgf(Omega_m: float, Omega_lambda: float, z: float, H0: float = 67.36) -> float:
+def linear_growth_factor(Omega_m: float, Omega_lambda: float, z: float, H0: float = 67.36) -> float:
     '''
     Compute the linear growth factor of a cosmology at redshift z
 
     Parameters
     ----------
     Omega_m: float
-        Proportion of matter component
+        Proportion of matter component today
     Omega_lambda: float
-        Proportion of dark energy component
+        Proportion of dark energy component today
     z: float
         Redshift
     H0: float, optional
