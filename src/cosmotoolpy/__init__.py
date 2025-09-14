7799a17569bd88402517cfd6b848fdabf550e3f5
Jiang Mengtian <jiangmengtian@JiangdeMacBook-Pro.local>
2025-09-14 19:32:01 +0800

    Add function cross_power_spectrum_estimator

--- src/cosmotoolpy/__init__.py
+++ src/cosmotoolpy/__init__.py
@@ -1,5 +1,5 @@
 from .linear_growth_factor import linear_growth_factor
 from .power_spectrum import PowerSpectrum
-from .cpse import power_spectrum_estimator
+from .cpse import power_spectrum_estimator, cross_power_spectrum_estimator
 from .cpaint import ngp_interlace, cic_interlace
 from .zeldovich_approximation import zeldovich_approximation
--- src/cosmotoolpy/cpower_spectrum_estimator.pyx
+++ src/cosmotoolpy/cpower_spectrum_estimator.pyx
@@ -111,3 +111,73 @@ def power_spectrum_estimator(delta_k: np.ndarray, Ngrid: int, L: float = 1000) -
         return _power_spectrum_estimator_float64(delta_k, Ngrid, L)
     else:
         raise TypeError(f"Unsupported input type: {delta_k.dtype}")
+
+def _cross_power_spectrum_estimator_complex128(cnp.ndarray[cnp.complex128_t, ndim=3] delta_k1, cnp.ndarray[cnp.complex128_t, ndim=3] delta_k2, int Ngrid, double L) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    '''
+    Compute the cross power spectrum of two density contrast fields in Fourier space for type(delta_k) == np.complex128
+    '''
+    cdef double kF = 2*pi/L
+    cdef double V  = L*L*L
+
+    cdef int kx_size = delta_k1.shape[0]
+    cdef int ky_size = delta_k1.shape[1]
+    cdef int kz_size = delta_k1.shape[2]
+    cdef int nx_max = kx_size // 2
+    cdef int ny_max = ky_size // 2
+    cdef int nz_max = kz_size
+    cdef int n_max = <int>(round(sqrt(<double>(nx_max*nx_max+ny_max*ny_max+nz_max*nz_max)))+1)
+
+    cdef cnp.ndarray[cnp.float64_t, ndim=1] k = np.arange(n_max, dtype=np.float64) * kF
+    cdef cnp.ndarray[cnp.float64_t, ndim=1] P_k = np.zeros(n_max, dtype=np.float64)
+    cdef cnp.ndarray[cnp.int64_t, ndim=1] N_k = np.zeros(n_max, dtype=np.int64)
+    
+    cdef int i, j, m, index, nx, ny
+    cdef int kx_mid = (kx_size-1)//2
+    cdef int ky_mid = (ky_size-1)//2
+    cdef cnp.complex128_t[:, :, :] delta_k1_view = delta_k1
+    cdef cnp.complex128_t[:, :, :] delta_k2_view = delta_k2
+    for i in range(kx_size):
+        nx = i-kx_size if i>kx_mid else i
+        for j in range(ky_size):
+            ny = j-ky_size if j>ky_mid else j
+            for m in range(kz_size):
+                if (m==0 and (i>kx_mid or (i==0 and j>ky_mid))): 
+                    continue
+                index = <int>round(sqrt(<double>(nx*nx+ny*ny+m*m)))
+                N_k[index] += 1
+                product = delta_k1_view[i,j,m]*delta_k2_view[i,j,m].conjugate()
+                P_k[index] += (delta_k1_view[i,j,m]*delta_k2_view[i,j,m].conj()).real
+    for i in range(n_max):
+        if N_k[i]>0:
+            P_k[i] = P_k[i]/(V*N_k[i])
+
+    return k[1:], P_k[1:], N_k[1:]
+
+def cross_power_spectrum_estimator(delta_k1: np.ndarray, delta_k2: np.ndarray, Ngrid: int, L: float = 1000) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    '''
+    Compute the cross power spectrum of two density contrast fields in Fourier space
+
+    Parameters
+    ----------
+    delta_k1: 3d-array
+        Density contrast field in Fourier space
+    delta_k2: 3d-array
+        Density contrast field in Fourier space
+    Ngrid: int
+        Number of grids
+    L: float, optional
+        Box size
+
+    Returns
+    -------
+    k: 1d-array
+        Wavenumber of each bin
+    P_k: 1d-array
+        Averaged cross power spectrum of each bin
+    N_k: 1d-array
+        Number of independent modes in each bin
+    '''
+    if delta_k1.dtype == np.complex128 and delta_k2.dtype == np.complex128:
+        return _cross_power_spectrum_estimator_complex128(delta_k1, delta_k2, Ngrid, L)
+    else:
+        raise TypeError(f"Unsupported input type: {delta_k1.dtype}, {delta_k2.dtype}")
