49222fda9242cd2c8fef8e4be429da09fa1a2f36
Jiang Mengtian <jiangmengtian@JiangdeMacBook-Pro.local>
2025-09-30 10:46:59 +0800

    Update __init__

--- src/cosmotoolpy/__init__.py
+++ src/cosmotoolpy/__init__.py
@@ -3,3 +3,4 @@ from .power_spectrum import PowerSpectrum
 from .cpse import power_spectrum_estimator, cross_power_spectrum_estimator
 from .cpaint import ngp_interlace, cic_interlace
 from .zeldovich_approximation import zeldovich_approximation
+from .fourier_basis import fourier_basis
